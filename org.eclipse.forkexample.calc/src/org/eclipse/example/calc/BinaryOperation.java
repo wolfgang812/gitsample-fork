3f4d144366fcba851c7251104deda7adf97b58c1
Lars Vogel <Lars.Vogel@vogella.com>
2020-12-08 11:54:49 +0100

    Fork Änderung

--- org.eclipse.forkexample.calc/src/org/eclipse/example/calc/BinaryOperation.java
+++ org.eclipse.forkexample.calc/src/org/eclipse/example/calc/BinaryOperation.java
@@ -15,6 +15,7 @@ public interface BinaryOperation extends Operation {
 	 * hier fehlt eine beschreibung immer noch
 	 * @param arg1 first parameter
 	 * @param arg2 second parameter
+	 * 
 	 * @return result of binary operation 
 	 */ 
 	public float perform(float arg1, float arg2);
